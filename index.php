35268c5108b4cf571beb3bcd8a5733b72e68fa85
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2023-02-28 10:35:16 +0100

    Good for day1

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 require ('Card.php');
 session_start();
 
+if(!isset($card)){
 for($i=0;$i < 9;$i++){
     $card[$i] = new Card();
     $card[$i]->setId($i);
     $card[$i]->setImage($i.".png");
+//    $card[$i]->setState("hide");
 
+}
 }
 ?>
 
@@ -24,29 +27,43 @@ for($i=0;$i < 9;$i++){
 <?php
 
 foreach ($card as $key => $value){
-//    echo "<td>hello</td>";
-        echo '<tr><input type="submit" name="'.'card'.$key.'"> <img src="'.$card[$key]->getImage().'">'.'</input></tr>';
-if(!empty($_GET['card'.$key])){
-        if($_GET['card'.$key] == "Envoyer") {
-            $_SESSION['card' . $key] = $_GET['card' . $key];
-            if ($_GET['card' . $key]) {
-                echo '<tr><input type="submit" name="' . 'card' . $key . '"> <img src="back.jpg">' . '</input></tr>';
+    if(isset($_SESSION['card' . $key])){
+        $card[$key]->setState('visible');
+        echo '<tr><input type="submit" name="'.'card'.$key.'visible"> <img height="100px" src="'.$card[$key]->getImage().'">'.'</input></tr>';
 
-            } else {
-                echo '<tr><input type="submit" name="' . 'card' . $key . '"> <img src="' . $card[$key]->getImage() . '">' . '</input></tr>';
 
+    }else{
+        if(isset($_GET['card'.$key])){
+            if($_GET['card'.$key] == "Envoyer") {
+                $_SESSION['card' . $key] = $_GET['card' . $key];
+                header('location:index');
             }
         }
+        echo '<tr><input type="submit" name="' . 'card' . $key . '"> <img height="100px" src="back.jpg">' . '</input></tr>';
 
-
-        }
-//    for ($x=0; $x < 3; $x++) {
-//        echo "<td>";
-//
-//        echo "</td>";
-//    }
-
+    }
 }
+?>
+    <button type="submit" name="reset" value="reset">Reset la partie</button>
+        <?php
+resetGame();
+function resetGame(){
+if(isset($_GET['reset']))
+{
+    session_unset();
+    session_destroy();
+    unset($_GET);
+    header('location:index');
+}
+}
+
+
+var_dump($card);
+var_dump($_GET);
+//unset($_GET);
+//session_destroy();
+var_dump($_SESSION);
+
 
  ?>
     </form>
